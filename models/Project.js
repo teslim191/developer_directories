7639af6aa6b9b950d6d03fa6639d690f645373bc
unknown <teslimjimoh191@gmail.com>
2022-12-04 20:56:20 +0000

    created a yaml file

--- models/Project.js
+++ models/Project.js
@@ -1,30 +1,35 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const ProjectSchema = new mongoose.Schema({
+const ProjectSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     tool: {
-        type: Array,
-        required: true
+      type: Array,
+      required: true,
     },
-    start_date:{
-        type: Date,
-        required: true
+    start_date: {
+      type: Date,
+      required: true,
     },
     end_date: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     duration: {
-        type: String,
-        required: true
+      type: Number,
+      required: true,
     },
-    devs: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'Dev'
-    }
-})
+    dev: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Dev",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Project', ProjectSchema)
+module.exports = mongoose.model("Project", ProjectSchema);
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,6 @@ const Dev = require("../models/Dev");
 const { ensureAuth } = require("../middleware/auth");
 const Project = require("../models/Project");
 const moment = require("moment");
-const { duration } = require("moment");
 
 // get current loggedin developer
 router.get("/me", ensureAuth, async (req, res) => {
@@ -13,6 +12,7 @@ router.get("/me", ensureAuth, async (req, res) => {
     res.status(200).json({
       name: dev.name,
       email: dev.email,
+      job_title: dev.job_title
     });
   } catch (error) {
     res.status(500).json({ error: "server error" });
@@ -22,16 +22,16 @@ router.get("/me", ensureAuth, async (req, res) => {
 // get all developers in the database
 router.get("/developers", ensureAuth, async (req, res) => {
   const pages = req.query.pages || 0;
-  const devsPerPage = 5;
+  const devPerPage = 5;
   try {
-    let devs = await Dev.find()
+    let dev = await Dev.find()
       .select(" -password ")
-      .skip(pages * devsPerPage)
-      .limit(devsPerPage);
-    if (!devs) {
+      .skip(pages * devPerPage)
+      .limit(devPerPage);
+    if (!dev) {
       res.status(400).json({ message: "No Developer found" });
     } else {
-      res.status(200).json(devs);
+      res.status(200).json(dev);
     }
   } catch (error) {
     res.status(500).json({ error: "server error" });
@@ -50,7 +50,7 @@ router.get("/developer/:id", ensureAuth, async (req, res) => {
       res.status(200).json(dev);
     }
   } catch (error) {
-    console.log(error);
+    res.status(500).json({error: 'server error'})
   }
 });
 
@@ -67,30 +67,32 @@ router.post("/project", ensureAuth, async (req, res) => {
       tool,
       start_date: startDate,
       end_date: endDate,
-      duration: `${diff} days`,
-      devs: req.dev.id,
+      duration: diff,
+      dev: req.dev.id,
     });
     res.status(201).json({
+      _id: project._id,
       title: project.title,
       tool: project.tool,
       start_date: project.start_date,
       end_date: project.end_date,
-      duration: project.duration,
-      devs: req.dev.name,
+      duration: `${project.duration} days`,
+      dev: req.dev.name,
     });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({error: 'server error'})
   }
 });
 
-// get all projects of single developer
-router.get("/projects", ensureAuth, async (req, res) => {
+// get projects of all developer
+router.get("/projects/all", ensureAuth, async (req, res) => {
   const pages = req.query.pages || 0;
   const projectsPerPage = 5;
   try {
-    let projects = await Project.find({ devs: req.dev.id })
-    .lean()
-      .populate('devs','name job_title')
+    let projects = await Project.find()
+      .lean()
+      .sort({ createdAt: -1 }) 
+      .populate("dev", "name job_title")
       // .select(" -_id -email -password -location -createdAt -updatedAt")
       .skip(pages * projectsPerPage)
       .limit(projectsPerPage);
@@ -102,7 +104,31 @@ router.get("/projects", ensureAuth, async (req, res) => {
       // console.log(posts)
     }
   } catch (error) {
-    console.log(error);
+    res.status(500).json({error: 'server error'})
+  }
+});
+
+// get all projects of single developer
+router.get("/projects", ensureAuth, async (req, res) => {
+  const pages = req.query.pages || 0;
+  const projectsPerPage = 5;
+  try {
+    let projects = await Project.find({ dev: req.dev.id })
+      .lean()
+      .sort({ createdAt: -1 })
+      .populate("dev", "name job_title")
+      // .select(" -_id -email -password -location -createdAt -updatedAt")
+      .skip(pages * projectsPerPage)
+      .limit(projectsPerPage);
+    if (!projects) {
+      res.status(404).json({ error: "this developer has no project" });
+    } else {
+      res.status(200).json(projects);
+
+      // console.log(posts)
+    }
+  } catch (error) {
+    res.status(500).json({error: 'server error'})
   }
 });
 
@@ -110,23 +136,145 @@ router.get("/projects", ensureAuth, async (req, res) => {
 router.get("/project/:id", ensureAuth, async (req, res) => {
   try {
     let project = await Project.findById({ _id: req.params.id }).populate(
-      "devs"
+      "dev"
     );
     if (!project) {
       res.status(404).json({ error: "project does not exists" });
     } else {
       res.status(200).json({
+        _id: project._id,
+        title: project.title,
+        tool: project.tool,
+        start_date: project.start_date,
+        end_date: project.end_date,
+        duration: `${project.duration} days`,
+        dev: project.dev.name,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({error: 'server error'})
+  }
+});
+// edit a developer detail
+router.patch("/developer/:id", ensureAuth, async (req, res) => {
+  try {
+    let dev = await Dev.findById({ _id: req.params.id });
+    if (!dev) {
+      res.status(404).json({ error: "this developer does not exist" });
+    } else if (req.dev.id != dev._id) {
+      res.status(400).json({ error: "you are not authorized to edit this developer details" });
+    } else {
+      dev = await Dev.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      res.status(201).json(dev);
+    }
+  } catch (error) {
+    res.status(500).json({error: 'server error'})
+  }
+});
+
+// delete a developer
+router.delete("/developer/:id", ensureAuth, async (req, res) => {
+  try {
+    let dev = await Dev.findById({ _id: req.params.id });
+    if (!dev) {
+      res.status(404).json({ error: "developer does not exist" });
+    } else if (req.dev.id != dev._id) {
+      res.status(400).json({ error: "you are not authorized to delete this developer" });
+    } else {
+      dev = await Dev.findByIdAndDelete({ _id: req.params.id });
+      res.status(200).json({ message: dev.name + " deleted succesfully" });
+    }
+  } catch (error) {
+    res.status(500).json({error: 'server error'})
+  }
+});
+
+// edit a project
+router.patch("/project/:id", ensureAuth, async (req, res) => {
+  try {
+    let project = await Project.findById({ _id: req.params.id }).populate("dev");
+    if (!project) {
+      res.status(404).json({ error: "this project does not exist" });
+    } else if (req.dev.id != project.dev._id) {
+      res.status(400).json({ error: "you are not authorized to edit this project details" });
+    } else {
+      project = await Project.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      res.status(201).json({
         title: project.title,
         tool: project.tool,
         start_date: project.start_date,
         end_date: project.end_date,
         duration: project.duration,
-        devs: project.devs.name,
+        dev: project.dev.name,
       });
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
+    // res.status(500).json({error: 'server error'})
   }
 });
+
+// delete a project
+router.delete("/project/:id", ensureAuth, async (req, res) => {
+  try {
+    let project = await Project.findById({ _id: req.params.id }).populate('dev');
+    if (!project) {
+      res.status(404).json({ error: "project does not exist" });
+    } else if (req.dev.id != project.dev._id) {
+      res.status(400).json({ error: "you are not authorized to delete this project document" });
+    } else {
+      project = await Project.findByIdAndDelete({ _id: req.params.id });
+      res.status(200).json({ message: project.title+" deleted succesfully" });
+    }
+  } catch (error) {
+    // console.log(error)
+    res.status(500).json({error: 'server error'});
+  }
+});
+
+// get stats
+// router.get('/stats', async (req, res) => {
+//   let stats = await Project.aggregate([
+//     {
+//       $group:{
+//         duration: {$max: 1}
+//       }
+//     }
+//   ])
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
